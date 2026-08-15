4fd3cad20c6115d89f9dbd73138abc698b84bf80
William Felix <william.felix059@gmail.com>
2026-08-15 16:09:02 +0700

    fix: assign gemini client instead of annotating

--- src/core/llm.py
+++ src/core/llm.py
@@ -4,8 +4,6 @@ from google import genai
 
 @lru_cache(maxsize=1)
 def get_gemini_client():
-    gemini_client: genai.Client(
-        api_key= env.GEMINI_API_KEY
-    )
+    gemini_client = genai.Client(api_key=env.GEMINI_API_KEY)
 
     return gemini_client
